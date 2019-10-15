5c0d33e2858dac1e1305505e6b753280b05b7a80
gabriella.plaia <gabriella.plaia@verizon.net>
2019-10-15 17:58:56 -0400

    added Phone class with pseudocode/outline of functions

---
+++ JavaApplication54/src/javaapplication54/Phone.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication54;
+
+/**
+ *
+ * @author Gab
+ */
+public class Phone {
+     //add class objects here to access their functions
+    
+     /*
+    void printStatement -- no arguments
+        print: you try the phone, but there are keys missing 57136
+        What do you want to do?
+        get userinput 
+        call CodeBookshelfPainting function with userInput as argument
+        
+    */
+    
+    
+    /*
+    void CodeBookshelfPainting(String u)
+    {
+        //make sure to add keywords: CODE, BOOKSHELF, PAINTING, and code word WAIKA to key word list in Room1 file!
+        if user entered CODE
+            call CODE 
+         else if user entered BOOKSHELF
+            call bookshelf
+         else if user entered PAINTING
+            call painting
+         else if user entered TIMER
+            call timer 
+            call print statement of this class again
+         else
+            ERROR we dont recognize this word
+            call print statemnt of this class again
+    }
+    */
+}
