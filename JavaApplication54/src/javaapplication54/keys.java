ef09cba56f48c92c8e1b30814237581f4624bf11
gabriella.plaia <gabriella.plaia@verizon.net>
2019-10-27 18:59:11 -0400

    added keys file, which contains enum keys which are the keywords the user will be entering

---
+++ JavaApplication54/src/javaapplication54/keys.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication54;
+
+/**
+ * 
+ * @author Gab
+ * keys file contains all key words used in the rooms. 
+ * 
+ * 
+ */
+        enum keys { 
+        MOVE, WAIT, LEFT, RIGHT, THROW, SMELL, FORWARD, EXPLORE, CODE, PAINTING, TIMER, BOOKSHELF, WAIKA, PHONE, LAMP;  
+    }
