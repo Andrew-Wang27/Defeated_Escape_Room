8a5f859e2d8014cf0faa6a37b798af6c337a28d8
gabriella.plaia <gabriella.plaia@verizon.net>
2019-11-26 12:26:27 -0500

    added QUIT statement

--- JavaApplication54/src/javaapplication54/Painting.java
+++ JavaApplication54/src/javaapplication54/Painting.java
@@ -27,7 +27,7 @@ public class Painting {
     {
         if(u == null)
         {
-            ui.printGameOutput("\nYou have given up by clicking CANCEL!\n"); 
+            ui.printGameOutput("\nYou have given up by clicking QUIT!\n"); 
         }
         
         else if(keys.CODE.name().equalsIgnoreCase(u))
