76ed0f04718051a58044f49a90869119632f75e3
gabriella.plaia <gabriella.plaia@verizon.net>
2019-11-29 16:52:46 -0500

    changed output messages to reflect room theme changes

--- JavaApplication54/src/javaapplication54/JavaApplication54.java
+++ JavaApplication54/src/javaapplication54/JavaApplication54.java
@@ -39,12 +39,12 @@ public class JavaApplication54 {
         
         
         //open room 1
-        //room1.OpeningScenario();
+        room1.OpeningScenario();
         //after room1 finishes, user moves to room2
         //room2.OpeningScenario(); 
         //after user complete room1 and room2, user moves to room3
         //room3.OpeningScenario();
-        maze.OpeningScenario();
+        
     }
     
  }
